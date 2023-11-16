e8ef396d4827da87bb17b45e7a157f34cdb56ba6
Your Name <zeeshannoor7831@gmail.com>
2023-11-16 21:28:24 +0500

    First Assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+// Question no 1   
+let person = "Zeeshan Noor";
+let age = 25;
+let student = true;
+console.log(person);
+console.log(age);
+console.log(student);
+// Question no 2
+let number1 = 15;
+let number2 = 10;
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+// Question no 3
+let myFavoriteLanguage = "Typescript";
+console.log(`My favorite programming language is "${myFavoriteLanguage}".`);
---
+++ index.ts
@@ -0,0 +1,24 @@
+// Question no 1   
+
+let person:string = "Zeeshan Noor";
+let age:number = 25;
+let student:boolean = true
+console.log(person);
+console.log(age);
+console.log(student);
+
+// Question no 2
+
+let number1:number = 15;
+let number2:number = 10;
+
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+
+// Question no 3
+
+let myFavoriteLanguage: string = "Typescript";
+
+console.log(`My favorite programming language is "${myFavoriteLanguage}".`);
