745c667e4248c61540898b2ba7571b6c241feef3
Your Name <zeeshannoor7831@gmail.com>
2024-01-28 22:41:09 +0500

    Number Guessing Game

--- index.js
+++ index.js
@@ -31,54 +31,101 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const chalk_1 = __importDefault(require("chalk"));
 const inquirer = __importStar(require("inquirer"));
-const exchangeRates = {
-    USD: 1,
-    EUR: 0.85,
-    PKR: 280, // Pakistan Rupee
+// //This program will take an amount in one currency and convert it to another currency based on a predefined exchange rate.
+// type ExchangeRates = Record<string, number>;
+// const exchangeRates: ExchangeRates = {
+//   USD: 1, // US Dollar
+//   EUR: 0.85, // Euro
+//   PKR: 280, // Pakistan Rupee
+// };
+// const promptCurrencyValue = async (): Promise<void> => {
+//   const questions = [
+//     {
+//       type: "input",
+//       name: "amount",
+//       message: "Enter the amount to convert:",
+//       validate: (value: string) => {
+//         const isValidNumber = !isNaN(parseFloat(value));
+//         return isValidNumber || "Please enter a valid number";
+//       },
+//     },
+//     {
+//       type: "input",
+//       name: "fromCurrency",
+//       message: "Enter the source currency code(e.g., USD):",
+//     },
+//     {
+//       type: "input",
+//       name: "toCurrency",
+//       message: "Enter the target currency code(e.g., PKR):",
+//     },
+//   ];
+//   const answers = await inquirer.prompt(questions);
+//   try {
+//     const amount = parseFloat(answers.amount);
+//     const convertedAmount = convert(
+//       amount,
+//       answers.fromCurrency.toUpperCase(),
+//       answers.toCurrency.toUpperCase()
+//     );
+//     console.log(convertedAmount);
+//   } catch (error: any) {
+//     console.error(error.message);
+//   }
+// };
+// function convert(
+//   amount: number,
+//   fromCurrency: string,
+//   toCurrency: string
+// ): string {
+//   if (!exchangeRates[fromCurrency] || !exchangeRates[toCurrency]) {
+//     throw new Error("Invalid currency code");
+//   }
+//   const rate = exchangeRates[toCurrency] / exchangeRates[fromCurrency];
+//   return `${chalk.yellow(amount + " " + fromCurrency)} is ${chalk.red(
+//     (amount * rate).toFixed(2) + toCurrency
+//   )}`;
+// }
+// promptCurrencyValue();
+const generateRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-const promptCurrencyValue = () => __awaiter(void 0, void 0, void 0, function* () {
-    const questions = [
+const getUserGuessNumber = () => __awaiter(void 0, void 0, void 0, function* () {
+    const { userGuess } = yield inquirer.prompt([
         {
             type: "input",
-            name: "amount",
-            message: "Enter the amount to convert:",
-            validate: (value) => {
-                const isValidNumber = !isNaN(parseFloat(value));
-                return isValidNumber || "Please enter a valid number";
+            name: "userGuess",
+            message: "Enter your guess:",
+            validate: (input) => {
+                const parsedInput = parseInt(input, 10);
+                return !isNaN(parsedInput) || "Please enter a valid number.";
             },
         },
-        {
-            type: "input",
-            name: "fromCurrency",
-            message: "Enter the source currency code(e.g., USD):",
-        },
-        {
-            type: "input",
-            name: "toCurrency",
-            message: "Enter the target currency code(e.g., PKR):",
-        },
-    ];
-    const answers = yield inquirer.prompt(questions);
-    try {
-        const amount = parseFloat(answers.amount);
-        const convertedAmount = convert(amount, answers.fromCurrency.toUpperCase(), answers.toCurrency.toUpperCase());
-        console.log(convertedAmount);
-    }
-    catch (error) {
-        console.error(error.message);
+    ]);
+    return userGuess;
+});
+const playNumberGuessingGame = () => __awaiter(void 0, void 0, void 0, function* () {
+    const minNumber = 1;
+    const maxNumber = 100;
+    const targetNumber = generateRandomNumber(minNumber, maxNumber);
+    let attempts = 0;
+    console.log("Welcome to the Number Guessing Game!");
+    console.log(`I'm thinking of a number between ${minNumber} and ${maxNumber}.`);
+    while (true) {
+        const guess = yield getUserGuessNumber();
+        attempts++;
+        if (guess < targetNumber) {
+            console.log("Too low! Try again.");
+        }
+        else if (guess > targetNumber) {
+            console.log("Too high! Try again.");
+        }
+        else {
+            console.log(`Congratulations! You guessed the number ${targetNumber} in ${attempts} attempts.`);
+            break;
+        }
     }
 });
-function convert(amount, fromCurrency, toCurrency) {
-    if (!exchangeRates[fromCurrency] || !exchangeRates[toCurrency]) {
-        throw new Error("Invalid currency code");
-    }
-    const rate = exchangeRates[toCurrency] / exchangeRates[fromCurrency];
-    return `${chalk_1.default.yellow(amount + " " + fromCurrency)} is ${chalk_1.default.red((amount * rate).toFixed(2) + toCurrency)}`;
-}
-promptCurrencyValue();
+playNumberGuessingGame();
--- index.ts
+++ index.ts
@@ -1,66 +1,116 @@
 import chalk from "chalk";
 import * as inquirer from "inquirer";
 
-//This program will take an amount in one currency and convert it to another currency based on a predefined exchange rate.
-type ExchangeRates = Record<string, number>;
+// //This program will take an amount in one currency and convert it to another currency based on a predefined exchange rate.
+// type ExchangeRates = Record<string, number>;
 
-const exchangeRates: ExchangeRates = {
-  USD: 1, // US Dollar
-  EUR: 0.85, // Euro
-  PKR: 280, // Pakistan Rupee
+// const exchangeRates: ExchangeRates = {
+//   USD: 1, // US Dollar
+//   EUR: 0.85, // Euro
+//   PKR: 280, // Pakistan Rupee
+// };
+
+// const promptCurrencyValue = async (): Promise<void> => {
+//   const questions = [
+//     {
+//       type: "input",
+//       name: "amount",
+//       message: "Enter the amount to convert:",
+//       validate: (value: string) => {
+//         const isValidNumber = !isNaN(parseFloat(value));
+//         return isValidNumber || "Please enter a valid number";
+//       },
+//     },
+//     {
+//       type: "input",
+//       name: "fromCurrency",
+//       message: "Enter the source currency code(e.g., USD):",
+//     },
+//     {
+//       type: "input",
+//       name: "toCurrency",
+//       message: "Enter the target currency code(e.g., PKR):",
+//     },
+//   ];
+
+//   const answers = await inquirer.prompt(questions);
+
+//   try {
+//     const amount = parseFloat(answers.amount);
+//     const convertedAmount = convert(
+//       amount,
+//       answers.fromCurrency.toUpperCase(),
+//       answers.toCurrency.toUpperCase()
+//     );
+//     console.log(convertedAmount);
+//   } catch (error: any) {
+//     console.error(error.message);
+//   }
+// };
+
+// function convert(
+//   amount: number,
+//   fromCurrency: string,
+//   toCurrency: string
+// ): string {
+//   if (!exchangeRates[fromCurrency] || !exchangeRates[toCurrency]) {
+//     throw new Error("Invalid currency code");
+//   }
+
+//   const rate = exchangeRates[toCurrency] / exchangeRates[fromCurrency];
+//   return `${chalk.yellow(amount + " " + fromCurrency)} is ${chalk.red(
+//     (amount * rate).toFixed(2) + toCurrency
+//   )}`;
+// }
+
+// promptCurrencyValue();
+
+// Number Guessing Game
+const generateRandomNumber = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const promptCurrencyValue = async (): Promise<void> => {
-  const questions = [
+const getUserGuessNumber = async (): Promise<number> => {
+  const { userGuess } = await inquirer.prompt([
     {
       type: "input",
-      name: "amount",
-      message: "Enter the amount to convert:",
-      validate: (value: string) => {
-        const isValidNumber = !isNaN(parseFloat(value));
-        return isValidNumber || "Please enter a valid number";
+      name: "userGuess",
+      message: "Enter your guess:",
+      validate: (input: string) => {
+        const parsedInput = parseInt(input, 10);
+        return !isNaN(parsedInput) || "Please enter a valid number.";
       },
     },
-    {
-      type: "input",
-      name: "fromCurrency",
-      message: "Enter the source currency code(e.g., USD):",
-    },
-    {
-      type: "input",
-      name: "toCurrency",
-      message: "Enter the target currency code(e.g., PKR):",
-    },
-  ];
+  ]);
 
-  const answers = await inquirer.prompt(questions);
+  return userGuess
+};
+const playNumberGuessingGame = async (): Promise<void> => {
+  const minNumber = 1;
+  const maxNumber = 100;
+  const targetNumber = generateRandomNumber(minNumber, maxNumber);
+  let attempts = 0;
 
-  try {
-    const amount = parseFloat(answers.amount);
-    const convertedAmount = convert(
-      amount,
-      answers.fromCurrency.toUpperCase(),
-      answers.toCurrency.toUpperCase()
-    );
-    console.log(convertedAmount);
-  } catch (error: any) {
-    console.error(error.message);
+  console.log("Welcome to the Number Guessing Game!");
+  console.log(
+    `I'm thinking of a number between ${minNumber} and ${maxNumber}.`
+  );
+
+  while (true) {
+    const guess = await getUserGuessNumber();
+    attempts++;
+
+    if (guess < targetNumber) {
+      console.log("Too low! Try again.");
+    } else if (guess > targetNumber) {
+      console.log("Too high! Try again.");
+    } else {
+      console.log(
+        `Congratulations! You guessed the number ${targetNumber} in ${attempts} attempts.`
+      );
+      break;
+    }
   }
 };
 
-function convert(
-  amount: number,
-  fromCurrency: string,
-  toCurrency: string
-): string {
-  if (!exchangeRates[fromCurrency] || !exchangeRates[toCurrency]) {
-    throw new Error("Invalid currency code");
-  }
-
-  const rate = exchangeRates[toCurrency] / exchangeRates[fromCurrency];
-  return `${chalk.yellow(amount + " " + fromCurrency)} is ${chalk.red(
-    (amount * rate).toFixed(2) + toCurrency
-  )}`;
-}
-
-promptCurrencyValue();
+playNumberGuessingGame();
